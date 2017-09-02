3e97ee86b4c6b4dfc52fd6103b8900f5736496e4
ANUSUYACHINNASAMY <canusuya05@gmail.com>
2017-09-02 11:43:07 +0530

    Create find num type.c

---
+++ find num type.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {
+		int input;
+scanf("input%d:",&input);
+if(input>0)
+{
+printf("positive");
+}
+else if(input<0)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}
+	return 0;
+}
+ 
